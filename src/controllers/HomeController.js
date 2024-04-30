ed393a5443ea47496c2368e02f822357f4e785e7
Eddy F_____ <31865186+Died1@users.noreply.github.com>
2024-04-30 14:17:43 -0400

    dia

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,28 +1,48 @@
-
-const Room = require('./../models/Room');
+const Room = require("./../models/Room");
 
 class HomeController {
-   async  index(req, res) {
-        let user = null;
-        let rooms = [];
-        if (req.session) {
-            user = req.session;
-            var creator =  user.user ?? null;
-            if (creator) {
-                rooms = await Room.find({ creator: creator._id });                
-            }
-        }
-        res.render('home/index', { user, rooms })
+  async index(req, res) {
+    let user = null;
+    let rooms = [];
+    if (req.session) {
+      user = req.session;
+      var creator = user.user ?? null;
+      if (creator) {
+        rooms = await Room.find({ creator: creator._id });
+      }
     }
-     getRoomsByCreatorId = async(creatorId) => {
-        try {
-            const rooms = await Room.find({ creator: creatorId });
-            return rooms;
-        } catch (error) {
-            console.error('Error al obtener las habitaciones:', error);
-            throw error;
-        }
+    res.render("home/index", { user, rooms });
+  }
+  getRoomsByCreatorId = async (creatorId) => {
+    try {
+      const rooms = await Room.find({ creator: creatorId });
+      return rooms;
+    } catch (error) {
+      console.error("Error al obtener las habitaciones:", error);
+      throw error;
     }
+  };
+
+  async getRoomsShared(req, res) {
+    try {
+      let user = null;
+      let rooms = [];
+      if (req.session) {
+        user = req.session;
+        var creator = user.user ?? null;
+        if (creator.email) {
+            rooms = await Room.find({
+              creator: { $ne: creator._id },
+              "members.email": creator.email,
+            });
+        }
+      }
+       res.render("home/shared", { user, rooms });
+    } catch (error) {
+      console.error("Error al obtener las habitaciones:", error);
+      throw error;
+    }
+  }
 }
 
 module.exports = new HomeController();
--- src/routes/dist/web.dev.js
+++ src/routes/dist/web.dev.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const parseContentTypeMiddleware = require('../middlewares/parseContentTypeMiddleware');
-const authMiddleware = require('../middlewares/authMiddleware');
+"use strict";
 
-const HomeController = require('../controllers/HomeController');
-const RoomController = require('../controllers/RoomController');
-const AuthController = require('../controllers/AuthController');
-const middlewares = [
-    parseContentTypeMiddleware,
-    authMiddleware,
-];
+var express = require('express');
 
-const router = express.Router();
+var parseContentTypeMiddleware = require('../middlewares/parseContentTypeMiddleware');
 
-router.get('/',  middlewares, HomeController.index);
+var authMiddleware = require('../middlewares/authMiddleware');
+
+var HomeController = require('../controllers/HomeController');
+
+var RoomController = require('../controllers/RoomController');
+
+var AuthController = require('../controllers/AuthController');
+
+var middlewares = [parseContentTypeMiddleware, authMiddleware];
+var router = express.Router();
+router.get('/', middlewares, HomeController.index);
 router.get('/home', middlewares, HomeController.index);
 router.get("/shared", middlewares, HomeController.getRoomsShared);
 router.get('/room/:roomID', middlewares, RoomController.index);
 router.post('/room/new', middlewares, RoomController.newRoom);
 router.post('/room/join', middlewares, RoomController.joinRoom);
 router.get('/room/lobby', middlewares, RoomController.lobby);
-router.post('/room/save',  RoomController.save);
-router.post('/room/export', RoomController.export);
-router.post('/room/gen_code',  RoomController.genCode);
-router.post('/room/add_colab', middlewares, RoomController.addColab)
+router.post('/room/save', RoomController.save);
+router.post('/room/export', RoomController["export"]);
+router.post('/room/gen_code', RoomController.genCode);
+router.post('/room/add_colab', middlewares, RoomController.addColab);
 router.get('/login', parseContentTypeMiddleware, AuthController.login);
 router.post('/auth/login', parseContentTypeMiddleware, AuthController.verify);
 router.get('/register', parseContentTypeMiddleware, AuthController.register);
@@ -30,7 +32,6 @@ router.post('/auth/register', parseContentTypeMiddleware, AuthController.save);
 router.get('/logout', parseContentTypeMiddleware, AuthController.logout);
 router.post('/file', parseContentTypeMiddleware, RoomController.save);
 router.use('*', function (req, res) {
-    res.render('404')
+  res.render('404');
 });
-
 module.exports = router;
